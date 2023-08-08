4c39f6eb2d0fbb6dc2f7b1a33ae752e3bbbb2328
dwhddnjs <per1215@naver.com>
2023-08-08 17:40:25 +0900

    ✨ refresh token 추가 준비

---
+++ src/auth/strategies/at.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt } from 'passport-jwt';
+import { Strategy } from 'passport-local';
+import { JwtPayload } from '../types/jwtPayload.type';
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrkey: config.get<string>('SECRET_KEY'),
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    return payload;
+  }
+}
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -20,6 +20,9 @@ export class User {
   @Column()
   password: string;
 
+  @Column()
+  refreshToken: string;
+
   @CreateDateColumn()
   createdAt: Date;
 
