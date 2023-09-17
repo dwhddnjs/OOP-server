713bda2b0eaba946018ad86aff76553e1d61b8d8
dwhddnjs <per1215@naver.com>
2023-09-17 19:08:59 +0900

    ✨ 로컬 api 추가

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -1,19 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Module, LoggerService } from '@nestjs/common';
 import { CardController } from './card.controller';
 import { CardService } from './card.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HttpModule, HttpService } from '@nestjs/axios';
 
 @Module({
-  controllers: [CardController],
+  controllers: [
+    CardController,
+    HttpModule.registerAsync({
+      useFactory: () => ({
+        timeout: 5000,
+        maxRedirects: 5,
+      }),
+    }) as any,
+  ],
   providers: [
     CardService,
     PrismaService,
     JwtService,
     UserService,
     ConfigService,
+    // HttpService,
   ],
 })
 export class CardModule {}
--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -1,7 +1,10 @@
 import { ConfigService } from '@nestjs/config';
 import { UserService } from './../user/user.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { Injectable, LoggerService } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class CardService {
@@ -9,9 +12,24 @@ export class CardService {
     private readonly prismaService: PrismaService,
     private readonly userService: UserService,
     private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+    private readonly logger: LoggerService,
   ) {}
 
   async openPack(packId: number, userId: string) {
+    const randomDogCard = this.configService.get<string>('DOG_API_URL');
+
+    const data = await firstValueFrom(
+      this.httpService.get<any>(randomDogCard).pipe(
+        catchError((error: AxiosError) => {
+          this.logger.error(error.response.data);
+          throw 'An error happened!';
+        }),
+      ),
+    );
+
+    console.log('data: ', data);
+
     // return await this.prismaService.pack.deleteMany({
     //   where: {
     //     id: packId,
