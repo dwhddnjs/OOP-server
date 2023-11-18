fc8d84544d470aecd005b2f0e7ca3a57dbbb9fd6
dwhddnjs <per1215@naver.com>
2023-11-19 04:27:54 +0900

    ✨ news data 추가

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { NewsService } from './news.service';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('news')
-export class NewsController {}
+export class NewsController {
+  constructor(private readonly newsService: NewsService) {}
+
+  @Get()
+  getNews() {
+    return this.newsService.getNews();
+  }
+}
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,4 +1,32 @@
+import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class NewsService {}
+export class NewsService {
+  constructor(private readonly configService: ConfigService) {}
+
+  async getNews() {
+    const naverId = await this.configService.get<string>('NAVER_ID');
+    const naverSecret = await this.configService.get<string>('NAVER_SECRET');
+    const naverUrl = await this.configService.get<string>('NAVER_URL');
+
+    const data = await axios({
+      url: naverUrl,
+      method: 'get',
+
+      params: {
+        query: 'lck',
+        display: 10,
+        start: 1,
+        sort: 'date',
+      },
+      headers: {
+        'X-Naver-Client-Id': naverId,
+        'X-Naver-Client-Secret': naverSecret,
+      },
+    });
+
+    return data?.data.items;
+  }
+}
