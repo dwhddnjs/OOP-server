710d2685004af8fe3013b9f78504bd294ff21e2c
dwhddnjs <per1215@naver.com>
2023-11-29 01:25:08 +0900

    🎨 포트번호 env로 분리

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 declare const module: any;
 
 async function bootstrap() {
+  const PORT = process.env.PORT;
+
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3003, 'localhost');
+  await app.listen(PORT, 'localhost');
   console.log(`Application is running on: ${await app.getUrl()}`);
 
   if (module.hot) {
