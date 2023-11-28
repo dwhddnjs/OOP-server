e6e9806be8f55598107f0190e77c7c7f4b7e0c4b
dwhddnjs <per1215@naver.com>
2023-11-29 03:09:08 +0900

    ✨ 포트번호

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: 'http://localhost:3000', // 허용할 클라이언트 도메인
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  await app.listen(8080, 'localhost');
+  app.enableCors();
+  await app.listen(PORT, 'localhost');
   console.log(`Application is running on: ${await app.getUrl()}`);
 
   if (module.hot) {
