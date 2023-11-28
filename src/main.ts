727b97e3719069612d36d195a1cc17de8e94e40c
dwhddnjs <per1215@naver.com>
2023-11-29 02:55:02 +0900

    ✨ cors변경

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000', // 허용할 클라이언트 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(8080, 'localhost');
   console.log(`Application is running on: ${await app.getUrl()}`);
 
