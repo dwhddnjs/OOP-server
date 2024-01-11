5913a2f3b2ad7dfacb41fb1877fe937085555921
dwhddnjs <per1215@naver.com>
2024-01-12 06:48:42 +0900

    🔥 oath 전략 제거

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,18 +6,10 @@ import { JwtModule } from '@nestjs/jwt';
 import { AtStrategy } from './strategies/at.strategy';
 import { RtStrategy } from './strategies/rt.strategy';
 import { UserService } from 'src/user/user.service';
-import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
   imports: [PassportModule, JwtModule.register({})],
-  providers: [
-    AuthService,
-    UserService,
-    AtStrategy,
-    RtStrategy,
-    GoogleStrategy,
-    Logger,
-  ],
+  providers: [AuthService, UserService, AtStrategy, RtStrategy, Logger],
   controllers: [AuthController],
 })
 export class AuthModule {}
